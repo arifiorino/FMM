14924606bca2ae34e718bcd8d6a5bbe5e9eba373
Ari Fiorino <arifiorino@gmail.com>
2023-10-09 00:05:03 -0500

    Fixed bug

--- sim.py
+++ sim.py
@@ -1,6 +1,8 @@
 import cmath, math, random
 
-p = 100
+p = 20
+N = 1000
+n=int(math.log(N)/math.log(10))
 
 #Theorem 2.1
 def naive_potential(qs,zs,z):
@@ -18,6 +20,7 @@ def run_gravitational_me(Q,aks,z):
   return Q/z+sum(-k*aks[k-1]/pow(z,k+1) for k in range(1,p+1))
 
 
+'''
 zs=[(1,1),(-1,1),(1,-1),(-1,-1)]
 qs=[1,1,1,1]
 z=(10,10)
@@ -28,17 +31,20 @@ z=complex(*z)
 Q,aks = create_gravitational_me(qs,zs)
 print(run_gravitational_me(Q,aks,z))
 print(naive_gravitational(qs,zs,z))
+'''
 
 
 #Lemma 2.3
 def shift_outer_me(a0, aks, z0):
   return [sum(aks[k-1]*pow(z0,l-k)*math.comb(l-1,k-1) for k in range(1,l+1)) - a0*pow(z0,l)/l for l in range(1,p+1)]
+'''
 aks=shift_outer_me(Q,aks,5+5j)
 print(run_gravitational_me(Q,aks,z))
 zs2=[(6,6),(4,6),(6,4),(4,4)]
 zs2=[complex(*x) for x in zs2]
 # we have aks == create_gravitational_me(qs,zs2)
 print(naive_gravitational(qs,zs2,z))
+'''
 
 
 #Lemma 2.4
@@ -50,6 +56,7 @@ def run_potential_inner_me(bs,z):
   return sum(bs[l]*pow(z,l) for l in range(p+1))
 def run_gravitational_inner_me(bs,z):
   return sum(l*bs[l]*pow(z,l-1) for l in range(p+1))
+'''
 _,aks = create_gravitational_me(qs,zs)
 aks=shift_inner_me(Q,aks,5+5j)
 z=1+0j
@@ -57,24 +64,18 @@ print(run_gravitational_inner_me(aks,z))
 zs2=[(6,6),(4,6),(6,4),(4,4)]
 zs2=[complex(*x) for x in zs2]
 print(naive_gravitational(qs,zs2,z))
+'''
 
 #Lemma 2.5
 def shift_power_series_center(aks,z0):
-  return [sum(aks[k]*math.comb(k,l)*pow(-z0,k-l) for k in range(l,p)) for l in range(p+1)]
+  return [sum(aks[k]*math.comb(k,l)*pow(-z0,k-l) for k in range(l,p+1)) for l in range(p+1)]
+'''
+aks=shift_power_series_center(aks,-1-1j)
+print(run_gravitational_inner_me(aks,-2-2j))
+_,aks = create_gravitational_me(qs,zs2)
+print(naive_gravitational(qs,zs2,-1-1j))
+'''
 
-#step 1: Apply theorem 2.1 to finest boxes about their centers
-#step 2: Add and shift for larger boxes (about their centers)
-#step 3: Calculate local expansion of everything in the interaction list first (Lemma 2.4).
-#        Now add that to the tilde Psi already calculated for parent
-#        Calculate tilde Psi for children by shifting using Lemma 2.5
-#step 4: Step 3 for final level
-#step 5: Evaluate Psi for layer n
-#step 6: Manually calculate local points
-#step 7: Add step 5 and 6
-
-
-
-n,N=2,150
 class Box():
   def __init__(self,level,i,j):
     self.level,self.i,self.j,self.particles=level,i,j,[]
@@ -114,8 +115,18 @@ def get_interaction_list(level,i,j):
 print('Step 1')
 for row in boxes[-1]:
   for box in row:
-    box.Q, box.Phi = create_gravitational_me([1 for _ in range(N)],
+    box.Q, box.Phi = create_gravitational_me([1 for _ in range(len(box.particles))],
                                              [x-box.center for x in box.particles])
+
+'''
+tmp=boxes[-1][-1][-1]
+c=-1-1j
+#print(tmp.center,c-tmp.center)
+print(run_gravitational_me(tmp.Q,tmp.Phi,c-tmp.center))
+#print([x+tmp.center for x in tmp.particles])
+print(naive_gravitational([1]*len(tmp.particles),tmp.particles,c))
+'''
+
 #Step 2
 print('Step 2')
 for level in range(n-1,-1,-1):
@@ -124,9 +135,19 @@ for level in range(n-1,-1,-1):
       for di in range(2):
         for dj in range(2):
           child = boxes[level+1][box.i*2+di][box.j*2+dj]
-          shifted=shift_outer_me(child.Q, child.Phi, box.center-child.center)
+          shifted=shift_outer_me(child.Q, child.Phi, child.center-box.center)
           box.Phi=[x+y for x,y in zip(box.Phi,shifted)]
           box.Q+=child.Q
+
+'''
+tmp=boxes[-2][-1][-1]
+c=-1-1j
+print(run_gravitational_me(tmp.Q,tmp.Phi,c-tmp.center))
+tmp2=boxes[-1][-1][-1].particles+boxes[-1][-1][-2].particles+boxes[-1][-2][-1].particles+boxes[-1][-2][-2].particles
+print(naive_gravitational([1]*len(tmp2),tmp2,c))
+'''
+
+
 
 #Step 3
 print('Step 3')
@@ -143,7 +164,7 @@ for level in range(1,n):
       for di in range(2):
         for dj in range(2):
           child = boxes[level+1][box.i*2+di][box.j*2+dj]
-          shifted = shift_power_series_center(box.Psi, box.center-child.center) #not sure
+          shifted = shift_power_series_center(box.Psi, child.center-box.center) #not sure
           child.Psi2=shifted
 
 #Step 4
@@ -173,19 +194,28 @@ for row in boxes[n]:
 
 #Step 6 + 7
 print('Step 6 + 7')
+particles_final=[]
+forces_final=[]
 for row in boxes[n]:
   for box in row:
     for i,x in enumerate(box.particles):
-      box.forces[i]+=run_gravitational_inner_me(box.Psi,x)
+      box.forces[i]+=run_gravitational_inner_me(box.Psi,x-box.center)
+      particles_final.append(x)
+      forces_final.append(box.forces[i])
 
 
-particles=[complex(*x) for x in particles]
-particle=boxes[n][0][0].particles[0]
-print(boxes[n][0][0].forces[0])
-particles2=set(particles)
-particles2.remove(particle)
-print(naive_gravitational([1]*len(particles2),particles2,particle))
-                                            
+print(len(particles_final),len(forces_final))
+forces_manual=[]
+particles_set=set(particles_final)
+for particle in particles_final:
+  particles_set.remove(particle)
+  forces_manual.append(naive_gravitational([1]*len(particles_set),particles_set,particle))
+  particles_set.add(particle)
+
+for _ in range(5):
+  i=random.randint(0,N-1)
+  print(forces_final[i],'=?',forces_manual[i])
+
 
 
 
